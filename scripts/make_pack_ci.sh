9f0324d79671bb6ca2207807f2ae743a9feacd94
Daniele <oppifjellet@gmail.com>
2022-02-10 02:24:32 +0530

    add make pack script for CI jobs

---
+++ scripts/make_pack_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cwd=${PWD}
+printf '%s %s\n' "${PWD}" "$1"
+cd ./packs/$1
+name=$1-$(jq -r .version ./manifest)
+zip -r -1 $name.l5rcmpack ./* -x *.git*
+mv $name.l5rcmpack $cwd
+cd $cwd
+
